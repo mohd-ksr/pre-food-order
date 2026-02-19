354c532280010a7ff722092555800ad3ef4aa688
Mo Kausar <153416833+mohd-ksr@users.noreply.github.com>
2026-02-19 11:36:55 +0530

    Restrict CORS origins to specific domains

--- backend/app/core/middleware.py
+++ backend/app/core/middleware.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 def setup_middleware(app):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        allow_origins=["https://pre-food-order.netlify.app","https://pre-food-order-admin.netlify.app"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
